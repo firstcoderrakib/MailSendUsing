29d838099d125fd24006c3594e4a2f473ea4d610
Rakib Islam <rakib.shehab@gmail.com>
2020-01-11 21:22:56 +0600

    First

---
+++ MailSend/MailSend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MailSend.MvcApplication" Language="C#" %>
---
+++ MailSend/MailSend/Models/gamil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MailSend.Models
+{
+    public class gamil
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
