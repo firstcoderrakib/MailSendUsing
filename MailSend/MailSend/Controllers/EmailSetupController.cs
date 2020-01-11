a404a56d74aff292e8cd308ed83b8f4dace85dd3
Rakib Islam <rakib.shehab@gmail.com>
2020-01-11 23:14:58 +0600

    last commit

---
+++ MailSend/MailSend/Controllers/EmailSetupController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MailSend.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailSend.Controllers
+{
+    public class EmailSetupController : Controller
+    {
+        // GET: EmailSetup
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(MailSend.Models.gamil model)
+        {
+            MailMessage mm = new MailMessage("rakibcse880@gmail.com", model.To);
+            mm.Subject = model.Subject;
+            mm.Body = model.Body;
+            mm.IsBodyHtml = false;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+
+            NetworkCredential nc = new NetworkCredential("rakibcse880@gmail.com","rakib");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = nc;
+            smtp.Send(mm);
+            ViewBag.Message ="Mail Has Been Sent Successfully"
+            return View();
+        }
+    }
+}
